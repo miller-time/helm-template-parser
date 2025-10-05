1c7dd571870c3a33451c3f6bf4cfca1195dbac48
Russell <miller.time.baby@gmail.com>
2025-10-05 08:04:51 -0700

    begin tokenizing

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,5 +1,55 @@
 use crate::{error::Error, token::Token};
 
-pub fn tokenize(_input: &str) -> Result<Vec<Token>, Error> {
-    Ok(Vec::new())
+pub fn tokenize(input: &str) -> Result<Vec<Token>, Error> {
+    let mut tokens = Vec::new();
+
+    let mut iter = input.chars();
+    loop {
+        match iter.next() {
+            Some(c) => {
+                let token = to_token(c)?;
+                tokens.push(token);
+            }
+            None => break,
+        }
+    }
+
+    Ok(tokens)
+}
+
+fn to_token(c: char) -> Result<Token, Error> {
+    match c {
+        ' ' => Ok(Token::Space),
+        '-' => Ok(Token::Dash),
+        ':' => Ok(Token::Colon),
+        '/' => Ok(Token::Slash),
+        '.' => Ok(Token::Dot),
+        '"' => Ok(Token::DoubleQuote),
+        '{' => Ok(Token::LeftBrace),
+        '}' => Ok(Token::RightBrace),
+        _ => Err(Error::LexerError(format!("unrecognized input '{}'", c))),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn to_token() {
+        let tokens = tokenize(" -:/.\"{}").expect("failed to tokenize");
+        assert_eq!(
+            tokens,
+            vec![
+                Token::Space,
+                Token::Dash,
+                Token::Colon,
+                Token::Slash,
+                Token::Dot,
+                Token::DoubleQuote,
+                Token::LeftBrace,
+                Token::RightBrace
+            ]
+        );
+    }
 }
--- src/token.rs
+++ src/token.rs
@@ -1,9 +1,13 @@
+#[derive(Debug, PartialEq)]
 pub enum Token {
     Space,
     Letter(char),
     Number(u8),
     Dash,
     Colon,
+    Slash,
+    Dot,
+    DoubleQuote,
     LeftBrace,
     RightBrace,
 }
