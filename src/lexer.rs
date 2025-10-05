52f281c5298cb424a9f88b1f33e606b769af669a
Russell <miller.time.baby@gmail.com>
2025-10-05 08:23:52 -0700

    add more tokens

--- src/lexer.rs
+++ src/lexer.rs
@@ -19,14 +19,28 @@ pub fn tokenize(input: &str) -> Result<Vec<Token>, Error> {
 
 fn to_token(c: char) -> Result<Token, Error> {
     match c {
+        letter if letter.is_alphabetic() => Ok(Token::Letter(c)),
+        number if number.is_numeric() => Ok(Token::Number(c as u8)),
         ' ' => Ok(Token::Space),
         '-' => Ok(Token::Dash),
         ':' => Ok(Token::Colon),
         '/' => Ok(Token::Slash),
+        '|' => Ok(Token::Pipe),
         '.' => Ok(Token::Dot),
+        ',' => Ok(Token::Comma),
+        '\'' => Ok(Token::SingleQuote),
         '"' => Ok(Token::DoubleQuote),
+        '*' => Ok(Token::Asterisk),
+        '=' => Ok(Token::Equals),
+        '%' => Ok(Token::Percent),
+        '\n' => Ok(Token::Newline),
+        '(' => Ok(Token::LeftParen),
+        ')' => Ok(Token::RightParen),
+        '[' => Ok(Token::LeftBracket),
+        ']' => Ok(Token::RightBracket),
         '{' => Ok(Token::LeftBrace),
         '}' => Ok(Token::RightBrace),
+        '$' => Ok(Token::Dollar),
         _ => Err(Error::LexerError(format!("unrecognized input '{}'", c))),
     }
 }
@@ -37,18 +51,32 @@ mod tests {
 
     #[test]
     fn to_token() {
-        let tokens = tokenize(" -:/.\"{}").expect("failed to tokenize");
+        let tokens = tokenize("a1 -:/|.,'\"*=%\n()[]{}$").expect("failed to tokenize");
         assert_eq!(
             tokens,
             vec![
+                Token::Letter('a'),
+                Token::Number(49),
                 Token::Space,
                 Token::Dash,
                 Token::Colon,
                 Token::Slash,
+                Token::Pipe,
                 Token::Dot,
+                Token::Comma,
+                Token::SingleQuote,
                 Token::DoubleQuote,
+                Token::Asterisk,
+                Token::Equals,
+                Token::Percent,
+                Token::Newline,
+                Token::LeftParen,
+                Token::RightParen,
+                Token::LeftBracket,
+                Token::RightBracket,
                 Token::LeftBrace,
-                Token::RightBrace
+                Token::RightBrace,
+                Token::Dollar,
             ]
         );
     }
