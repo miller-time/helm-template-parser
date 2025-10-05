712210e58cb690eba6b740392f853c80579f13c4
Russell <miller.time.baby@gmail.com>
2025-10-05 08:24:08 -0700

    update tokenize example

--- examples/tokenize/main.rs
+++ examples/tokenize/main.rs
@@ -3,8 +3,18 @@ use std::fs;
 use helm_template_parser::lexer::tokenize;
 
 fn main() {
-    let contents = fs::read_to_string("samples/clusterrole.yaml")
-        .expect("failed to open sample clusterrole.yaml");
-    let tokens = tokenize(&contents).expect("failed to tokenize");
-    dbg!(tokens);
+    let samples = fs::read_dir("samples").expect("failed to read samples directory");
+    for entry in samples {
+        let entry = entry.expect("failed to read sample");
+        let file_name = entry.file_name();
+        let file_name = file_name.to_str().expect("failed to read sample file name");
+        if !file_name.ends_with(".yaml") {
+            continue;
+        }
+        let contents = fs::read_to_string(entry.path())
+            .expect(&format!("failed to open sample {}", file_name));
+        let tokens =
+            tokenize(&contents).expect(&format!("failed to tokenize sample {}", file_name));
+        dbg!(file_name, tokens.len());
+    }
 }
